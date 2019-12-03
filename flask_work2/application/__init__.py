6af21a584e2d12c3d1f960588dac7ec511d99041
liammather97 <liammather97@gmail.com>
2019-12-03 16:47:50 +0000

    first commit

---
+++ flask_work2/application/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import pymysql
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from os import getenv
+
+app = Flask(__name__)
+
+# app.config['SECRET_KEY'] = '22g2h2kei478rtje8239rnr7'
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@35.246.86.15/flask_task'
+app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+pymysql://'+getenv('MYSQL_USER')+":"+getenv('MYSQL_PASSWORD')+"@"+getenv('MYSQL_HOST')+"/"+getenv('MYSQL_DB'))
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+from application import routes
+
